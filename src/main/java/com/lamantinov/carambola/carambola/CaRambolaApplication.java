fbf54ef6177deff8163b35be0f5b043181d99d55
Grigorii Lamantinov <grigoriiborodin@yandex.ru>
2022-05-12 20:14:17 +0300

    initial commit

---
+++ src/main/java/com/lamantinov/carambola/carambola/CaRambolaApplication.java
@@ -0,0 +1,13 @@
+package com.lamantinov.carambola.carambola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaRambolaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaRambolaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lamantinov/carambola/carambola/CaRambolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamantinov.carambola.carambola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaRambolaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
