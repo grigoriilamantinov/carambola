1cfef3fca0402bd72ff9b30a366fd8e3286d8de3
grigorii <grigoriiborodin@yandex.ru>
2022-06-02 22:31:09 +0300

    added ShopControllerIntegrationTest

--- src/test/java/com/lamantinov/carambola/carambola/features/shops/controllers/ShopControllerIntegrationTest.java
+++ src/test/java/com/lamantinov/carambola/carambola/features/shops/controllers/ShopControllerIntegrationTest.java
@@ -1,18 +1,10 @@
 package com.lamantinov.carambola.carambola.features.shops.controllers;
 
-import com.lamantinov.carambola.carambola.features.cars.dto.CarIntoShopsDTO;
-import com.lamantinov.carambola.carambola.features.cars.dto.CarWithoutShopsDTO;
-import com.lamantinov.carambola.carambola.features.cars.services.CarService;
-import com.lamantinov.carambola.carambola.features.shops.dto.ShopWithCarsDTO;
-import com.lamantinov.carambola.carambola.features.shops.dto.ShopWithoutCarsDTO;
-import com.lamantinov.carambola.carambola.features.shops.services.ShopService;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.lamantinov.carambola.carambola.features.owners.dto.OwnerWithoutCarsDTO;
-import com.lamantinov.carambola.carambola.features.owners.services.OwnerService;
+import com.lamantinov.carambola.carambola.features.cars.services.CarService;
+import com.lamantinov.carambola.carambola.features.shops.dto.ShopWithoutCarsDTO;
+import com.lamantinov.carambola.carambola.features.shops.services.ShopService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,11 +17,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -40,120 +30,81 @@ class ShopControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ShopService shopService;
-
     @Test
-    void shouldShowAllShops() throws Exception {
-        final String URL = "http://localhost:8080/api/shops/";
-        final ObjectMapper mapper = new ObjectMapper();
-        final String json = mockMvc.perform(
-                MockMvcRequestBuilders.get(URL)
-                    .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-
-        final List<ShopWithoutCarsDTO> exceptedResult = shopService.getAllWithoutCarsInfo();
-        final List<ShopWithoutCarsDTO> actualResult = mapper.readValue(json, new TypeReference<>(){});
-        Assertions.assertArrayEquals(new List[]{exceptedResult}, new List[]{actualResult});
-
-    }
-
-    @Test
-    void shouldGetShop() throws Exception {
-        final String URL = "http://localhost:8080/api/shops/2";
-
+    void shouldGetShops() throws Exception {
+        final String URL = "http://localhost:8080/shops/";
         mockMvc.perform(
                 MockMvcRequestBuilders.get(URL)
                     .contentType(MediaType.APPLICATION_JSON)
             )
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.id", is(2)))
-            .andExpect(jsonPath("$.shopName", is("BNW")))
-            .andExpect(jsonPath("$.address", is("Moskovsky prospect building 12")))
-            .andExpect(jsonPath("$.phone", is("8-812-345-23-12")))
-            .andExpect(jsonPath("$.email", is("bnw@bnw.com")));
-    }
-
-    @Test
-    void shouldGetShopWithCars() throws Exception {
-        final String URL = "http://localhost:8080/api/shops/2/cars";
-        final ObjectMapper mapper = new ObjectMapper();
-        final String json = mockMvc.perform(
-                MockMvcRequestBuilders.get(URL)
-                    .contentType(MediaType.APPLICATION_JSON)
-            )
             .andReturn()
             .getResponse()
             .getContentAsString();
-
-        final List<CarWithoutShopsDTO> carWithoutShopsDTOList = new ArrayList<>(){{
-                add(new CarWithoutShopsDTO(1, "Maddyson", 1984, 2000000));
-                add(new CarWithoutShopsDTO(2, "Lada", 2006, 900000));
-                add(new CarWithoutShopsDTO(4, "Lada", 1999, 700000));
-                add(new CarWithoutShopsDTO(6, "BMW", 2018, 7000000));
-            }};
-
-        final ShopWithCarsDTO exceptedResult = new ShopWithCarsDTO();
-        exceptedResult.setId(2);
-        exceptedResult.setShopName("BNW");
-        exceptedResult.setCarWithoutShopsDTO(carWithoutShopsDTOList);
-
-        final ShopWithCarsDTO actualResult = mapper.readValue(json, ShopWithCarsDTO.class);
-        Assertions.assertEquals(exceptedResult, actualResult);
     }
 
     @Test
-    void shouldAddNewShop() throws Exception {
-        final String URL = "http://localhost:8080/api/shops/3";
-        final ObjectMapper mapper = new ObjectMapper();
-        final String json = mockMvc.perform(
+    void shouldShowCars() throws Exception {
+        final String URL = "http://localhost:8080/shops/2/cars";
+        mockMvc.perform(
                 MockMvcRequestBuilders.get(URL)
                     .contentType(MediaType.APPLICATION_JSON)
             )
+            .andExpect(status().isOk())
             .andReturn()
             .getResponse()
             .getContentAsString();
-        final ShopWithoutCarsDTO shopWithoutShopsDTO = mapper.readValue(json, ShopWithoutCarsDTO.class);
-        final var exceptedResult = 3;
-        final var actualResult = shopWithoutShopsDTO.getId();
-        Assertions.assertEquals(exceptedResult, actualResult);
     }
 
     @Test
-    void shouldUpdateShop() throws Exception {
-        final String URL = "http://localhost:8080/api/shops/3";
-        final ObjectMapper mapper = new ObjectMapper();
-        final String json = mockMvc.perform(
+    void shouldShowShops() throws Exception {
+        final String URL = "http://localhost:8080/shops/2";
+        mockMvc.perform(
                 MockMvcRequestBuilders.get(URL)
                     .contentType(MediaType.APPLICATION_JSON)
             )
+            .andExpect(status().isOk())
             .andReturn()
             .getResponse()
             .getContentAsString();
-        final ShopWithoutCarsDTO shopWithoutShopsDTO = mapper.readValue(json, ShopWithoutCarsDTO.class);
-        final var exceptedResult = "Shop " + 3 + " was updated";
-        final var actualResult = "Shop " + shopWithoutShopsDTO.getId() + " was updated";
-        Assertions.assertEquals(exceptedResult, actualResult);
     }
 
     @Test
-    void shouldDeleteShop() throws Exception {
-        final String URL = "http://localhost:8080/api/shops/3";
-        final ObjectMapper mapper = new ObjectMapper();
-        final String json = mockMvc.perform(
+    void shouldDeleteCarFromShop() throws Exception {
+        final String URL = "http://localhost:8080/shops/2/car/2";
+        mockMvc.perform(
                 MockMvcRequestBuilders.get(URL)
                     .contentType(MediaType.APPLICATION_JSON)
             )
+            .andExpect(status().is4xxClientError())
             .andReturn()
             .getResponse()
             .getContentAsString();
+    }
 
-        final ShopWithoutCarsDTO shopWithoutCarsDTO = mapper.readValue(json, ShopWithoutCarsDTO.class);
-        final var exceptedResult = "Car with ID = " + 3 + " was deleted";
-        final var actualResult = "Car with ID = " + shopWithoutCarsDTO.getId() + " was deleted";
-        Assertions.assertEquals(exceptedResult, actualResult);
+    @Test
+    void shouldAddCarIntoShop() throws Exception {
+        final String URL = "http://localhost:8080/shops/2/add/2";
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(URL)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().is4xxClientError())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+    }
+
+    @Test
+    void shouldShowAddPage() throws Exception {
+        final String URL = "http://localhost:8080/shops/2/addPage";
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(URL)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
     }
 }
